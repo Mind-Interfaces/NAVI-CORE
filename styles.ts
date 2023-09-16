8ea75d5745312bb4c3a3e549b651b44b02a3cc33
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-09-16 18:38:35 -0400

    Add files via upload

---
+++ styles.ts
@@ -0,0 +1,23 @@
+const styles = {
+  container: {
+    label: 'DigitalHuman',
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    '& a, & a:active, & a:focus, & button, & button:active, & button:focus': {
+      outline: 'none'
+    }
+  },
+  avatar: {
+    label: 'avatar',
+    height: '100%',
+    width: '100%'
+  },
+  localVideo: {
+    width: 400,
+    height: 300,
+    display: 'none'
+  }
+}
+
+export default styles
