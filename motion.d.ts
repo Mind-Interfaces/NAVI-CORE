4d637b8ed918b722ab541105b4cc8e5cd985a95f
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-09-17 07:44:15 -0400

    Update motion.d.ts

--- motion.d.ts
+++ motion.d.ts
@@ -1,13 +1,12 @@
 declare module 'framer-motion' {
   interface CustomMotionMethods {
-    custom: any;
+    custom: any; // consider being more specific with the type if possible
   }
-  export const motion: typeof motion & CustomMotionMethods;
-}
-
-declare module 'framer-motion' {
+  
   export interface AnimatePresenceProps {
     // ... Your custom properties here
   }
+  
+  export const motion: typeof motion & CustomMotionMethods;
   export const AnimatePresence: React.FC<AnimatePresenceProps>;
 }
