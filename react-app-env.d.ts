687c504993315cca3aa425ba17963f47c4ffef9b
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-09-17 07:24:34 -0400

    Update react-app-env.d.ts

--- react-app-env.d.ts
+++ react-app-env.d.ts
@@ -19,3 +19,10 @@ declare module '*.json' {
   const value: any
   export default value
 }
+
+declare module "*.svg" {
+    import React from "react"
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+    const src: string
+    export default src
+}
